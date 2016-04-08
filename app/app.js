2b27b8be1a25433090b5386b2c1d6aa3cc2a3016
rmad17 <souravbasu17@gmail.com>
2016-04-08 17:17:37 +0530

    App up and running

--- app/app.js
+++ app/app.js
@@ -16,20 +16,11 @@ config(['$routeProvider', function($routeProvider) {
     var app = angular.module('myApp',[
     'ui.bootstrap',
     'myApp.view1',
-    'myApp.view2',
     'myApp.version']);
 
     app
     .config(['$routeProvider', function($routeProvider) {
       $routeProvider.
-        when('/view1', {
-            templateUrl: 'view1/view1.html',
-            controller: "View1Ctrl"
-        }).
-        when('/view2', {
-            templateUrl: 'view2/view2.html',
-            controller: "View2Ctrl"
-        }).
         otherwise({redirectTo: '/view1'});
     }])
 
